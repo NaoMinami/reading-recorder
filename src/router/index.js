be55f2fc6b6db3ed0d855d036e7a9f1b6b83f1bd
NaoMinami <48648434+NaoMinami@users.noreply.github.com>
2021-10-17 15:54:46 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import BookSearch from '../views/BookSearch.vue'
+import BookForm from '../views/BookForm.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: BookSearch
+  },
+  {
+    path: '/form',
+    name: 'Form',
+    component: BookForm
+  },
+  {
+    path: '*',
+    redirect: '/'
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
